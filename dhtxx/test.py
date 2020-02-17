6d9c9a31ba7cc3b94afab65cd12872908feabe19
Laurens Dewaele <dewaele.laurens@gmail.com>
2020-02-17 18:44:15 +0100

    debug

--- dhtxx/test.py
+++ dhtxx/test.py
@@ -2,13 +2,15 @@
 # sudo pip3 install Adafruit_DHT
 
 import Adafruit_DHT
+import time
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
 humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-while true:
+while True:
+    time.sleep(0.2)
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
     else:
